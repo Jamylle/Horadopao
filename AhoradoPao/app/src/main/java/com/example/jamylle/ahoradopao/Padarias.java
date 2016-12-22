bbba83cc50dcdc59f4df642876fa4f8fe7137d43
Jamylle <jamylle_fo@hotmail.com>
2016-12-22 00:23:22 -0300

    Adição do projeto (Android Studio)

---
+++ AhoradoPao/app/src/main/java/com/example/jamylle/ahoradopao/Padarias.java
@@ -0,0 +1,13 @@
+package com.example.jamylle.ahoradopao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Padarias extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_padarias);
+    }
+}
